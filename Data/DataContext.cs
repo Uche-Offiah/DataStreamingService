b5b39ba2e2985e7eb0d3a11922a7710266f94e23
Uchechukwu Offiah <evaristusoffiah@gmail.com>
2024-06-08 23:31:03 +0100

    Initial commit DataStreaming windows service

---
+++ Data/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using DataStreamingService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataStreamingService.Data
+{
+    public class DataContext : DbContext
+    {
+
+        private readonly IConfiguration _configuration;
+        public DataContext(IConfiguration config)
+        {
+            _configuration = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("GeodicInstance"));
+        }
+        public virtual DbSet<StreamData> StreamData { get; set; }
+    }
+}
---
+++ Data/DataRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStreamingService.Data
+{
+    public class DataRepository
+    {
+        private readonly DataContext _context;
+
+        public DataRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Data>> GetDataAsync()
+        {
+            return await _context.Data.ToListAsync();
+        }
+    }
+}
