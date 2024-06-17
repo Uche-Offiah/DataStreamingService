331b49914fc483c3a8df7d1eb1a771fab8f86529
Uchechukwu Offiah <evaristusoffiah@gmail.com>
2024-06-17 23:12:25 +0100

    Modified worker class to set url

--- Worker.cs
+++ Worker.cs
@@ -1,24 +1,62 @@
+using DataStreamingService.Services;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.WebSockets;
+
 namespace DataStreamingService
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly WebSocketHandler _webSocketHandler;
+        private readonly IConfiguration _configuration;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, WebSocketHandler webSocketHandler, IConfiguration configuration)
         {
             _logger = logger;
+            _webSocketHandler = webSocketHandler;
+            _configuration = configuration;
         }
+
+        //protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        //{
+        //    while (!stoppingToken.IsCancellationRequested)
+        //    {
+        //        if (_logger.IsEnabled(LogLevel.Information))
+        //        {
+        //            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+        //        }
+        //        await Task.Delay(1000, stoppingToken);
+        //    }
+        //}
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+            string url = _configuration.GetValue<string>("WebSocketServer:Url") ?? "http://localhost:5000/ws/";
+
+            var listener = new HttpListener();
+            listener.Prefixes.Add(url);
+            listener.Start();
+
             while (!stoppingToken.IsCancellationRequested)
             {
-                if (_logger.IsEnabled(LogLevel.Information))
+                var context = await listener.GetContextAsync();
+                if (context.Request.IsWebSocketRequest)
                 {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    await _webSocketHandler.HandleWebSocketAsync(context);
                 }
-                await Task.Delay(1000, stoppingToken);
+                else
+                {
+                    context.Response.StatusCode = 400;
+                    context.Response.Close();
+                }
             }
+
+            listener.Stop();
         }
     }
+
+}
 }
